9b05f5b8a078f694b6284425e67ebea00d997bbc
gleisonfernandes <gleison.fernandesb@gmail.com>
2023-05-14 15:32:02 -0300

    :recycle: refactor: Mudando o método da função pegaArquivo de then para async/await

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const trataErro = (erro) => {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
 }
 
-const pegaArquivo = (caminhoDoArquivo) => {
-    const encoding = 'utf-8';
-    fs.promises
-      .readFile(caminhoDoArquivo, encoding)
-      .then((texto) => log(chalk.green(texto)))
-      .catch(trataErro)
+const pegaArquivo = async (caminhoDoArquivo) => {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        log(chalk.green(texto));
+    }
+    catch(erro){
+        trataErro(erro);
+    }
 }
 
 pegaArquivo('./arquivos/texto.md');
