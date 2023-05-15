e2bac3b46b6f73b90ab5e28b683243a1be54a669
gleisonfernandes <gleison.fernandesb@gmail.com>
2023-05-15 10:26:32 -0300

    :sparkles: feat: Adicionando a possibilidade de ler diversos arquivos de um diretório e tratamento de erro

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,36 @@
+import fs from "fs";
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
 
 
-
 const caminho = process.argv;
 
-const processaTexto = async (caminho) =>{
-    const resultado = await pegaArquivo(caminho[2]);
+const imprimeLista = (resultado, nomeDoArquivo) => {
     console.log(chalk.yellow('Lista de Links'), resultado);
+}
+
+const processaTexto = async (argumentos) =>{
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if(erro.code === 'ENOENT'){
+            console.log(`Arquivo ou diretório não existe`);
+            return;
+        }
+    }
+
+    if(fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(resultado);
+    }else if(fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(lista);
+        })
+    }
 }
 
 processaTexto(caminho);
